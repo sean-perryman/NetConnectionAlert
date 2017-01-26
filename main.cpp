67bca443a378079ecec00bc066dd5a49701ccfcc
Sean Perryman <se@nperryman.com>
2017-01-25 19:25:06 -0800

    Working out getting the logic to work correctly.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@
 
 size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) { return size * nmemb; }
 
+//Init cURL stuff only once.
+CURL *curl;
+CURLcode res;
+ 
 bool ping() //From: https://curl.haxx.se/libcurl/c/simple.html
 {
-	CURL *curl;
-  CURLcode res;
- 
-  curl = curl_easy_init();
+	curl = curl_easy_init();
   if(curl) {
   	curl_easy_setopt(curl, CURLOPT_URL, "http://google.com");
     
@@ -51,7 +52,7 @@ int main(int argc, char ** argv)
 
 	while (!quit)
 	{
-		SDL_Delay(500);
+		SDL_Delay(2000);
 		SDL_PollEvent(&event);
 
 		switch (event.type)
@@ -63,11 +64,11 @@ int main(int argc, char ** argv)
 		
 		SDL_Rect dstrect = { 0, 0, 320, 320 };
 		SDL_SetTextureColorMod( texture, 255, 0, 0 );
-		SDL_RenderCopyEx(renderer, texture, NULL, &dstrect, rotation++, NULL, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(renderer, texture, NULL, &dstrect, rotation, NULL, SDL_FLIP_NONE);
 		SDL_RenderPresent(renderer);
 
-		if (ping()) rotation--;
-		else rotation++;
+		if (ping()) rotation = rotation + 18;
+		else rotation = 0;
 	}
 
 	SDL_DestroyTexture(texture);
